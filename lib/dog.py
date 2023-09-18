9630e9f6ca73fc16e9d75163f61fe481ac48c8c4
Nial Johnson <nialj707@gmail.com>
2023-09-17 21:04:35 -0700

    Complete

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
 
-class Dog:
-    pass
+class Dog():
+    def __init__(self, name, breed= "Mutt"):
+        self.name = name
+        self.breed = breed
+       
--- lib/person.py
+++ lib/person.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
 
-class Person:
-    pass
+class Person():
+   def __init__(self,name):
+      self.name = name
